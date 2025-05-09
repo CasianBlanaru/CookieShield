24dc5f5bc96b7f1ed0fbc4634cdcf28b05d46b32
Casian Blanaru <cab@tpwd.de>
2025-05-09 21:40:32 +0200

    Add Next.js configuration file for PWA support; integrate next-pwa, enable strict mode, and configure ESLint settings for production builds.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
+import NextPWA from 'next-pwa';
+
+const withPWA = NextPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
@@ -21,4 +23,4 @@ const nextConfig = {
 };
 
 // PWA-Konfiguration anwenden
-module.exports = withPWA(nextConfig); 
+export default withPWA(nextConfig); 
