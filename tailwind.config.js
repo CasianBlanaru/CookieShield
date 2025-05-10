93113cee961e1f8eba60b982da2cce1d42f63b35
Casian Blanaru <cab@tpwd.de>
2025-05-10 02:32:54 +0200

    Revert "Add Next.js configuration file with PWA support; enable strict mode, ESLint error handling, and CSS optimization for improved performance."
    This reverts commit fa202b0875bdde7c000a49ecffee7888e17233b2.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
       colors: {
@@ -45,11 +50,5 @@ const config = {
       },
     },
   },
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-};
-
-export default config; 
+  plugins: [],
+}; 
